181d904d867c50018e41b139d52667bb6b1c833f
Aida Abdikulova <abdikulova@gmail.com>
2019-10-23 06:41:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ module FormsLab
     end
     get '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      params[:pirate][:ships].each {|ship| Ship.new(ship}
+      params[:pirate][:ships].each {|ship| Ship.new(ship)}
       @ships = Ship.all
       erb :'pirates/show'
     end
